dbbdbc8beb3d91b8764dd0a38465e5b343ec1993
ArtemSherenkovskiy <superdyatel@gmail.com>
2016-10-12 11:30:01 +0300

    start hash func

--- mainserver.cpp
+++ mainserver.cpp
@@ -12,6 +12,29 @@ MainServer::MainServer():QTcpServer()
 
 }
 
+QString MainServer::hash(QString message)
+{
+    unsigned long long lenght = message.length() * CHAR_SIZE;
+    int toAdd = (((lenght % MESS_MOD) > MESS_MOD_RES) ? (MESS_MOD - ((lenght % MESS_MOD) % MESS_MOD_RES)) : (MESS_MOD_RES - (lenght % MESS_MOD)));
+    for(int i = 0; i < toAdd / CHAR_SIZE; i++)
+    {
+        message.append(" ");
+    }
+    int mess = MESS_LENGHT_SIZE;
+    for(int i = 1; i <= MESS_LENGHT_SIZE;)
+    {
+        char tmp = 0;
+        for(int j = 1; j <= CHAR_SIZE; j++, i++)
+        {
+            int a = sizeof(lenght);
+            tmp = tmp | (((lenght >> (MESS_LENGHT_SIZE - i)) & 1) << (CHAR_SIZE - j));
+        }
+        message.append(tmp);
+    }
+    qDebug() << "lenght " << lenght << " toAdd " << toAdd << " lenght " <<  message.length() * CHAR_SIZE << " message " << message;
+
+}
+
 void MainServer::processConenction()
 {
 
--- mainserver.h
+++ mainserver.h
@@ -3,11 +3,17 @@
 #include <QTcpServer>
 #include <QTcpSocket>
 #include <QDebug>
+#include <QString>
 class MainServer: public QTcpServer
 {
-
+private:
+    static const int CHAR_SIZE = 8;
+    static const int MESS_MOD = 512;
+    static const int MESS_MOD_RES = 448;
+    static const int MESS_LENGHT_SIZE = MESS_MOD - MESS_MOD_RES;
 public:
     MainServer();
+    static QString hash(QString message);
 public slots:
     void processConenction();
     void slotNewConnection();
